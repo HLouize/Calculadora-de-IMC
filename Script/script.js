5a454a1b2481e8da117364874b467029882ce753
Héllida Louize <hellida.llgomes@gmail.com>
2025-10-09 17:33:12 +0000

    script corrigido

--- Script/script.js
+++ Script/script.js
@@ -1,35 +1,31 @@
+
 function valorInvalido(peso, altura) {
     return isNaN(peso) || isNaN(altura) || peso < 0 || altura < 0;
 }
+document.getElementById("calcular").addEventListener("click", function () {
+const peso = parseFloat(document.getElementById("peso").value);
+const altura = parseFloat(document.getElementById("altura").value);
+const resultado =document.getElementById("resultado");
 
-let peso;
-let altura;
+if (valorInvalido(peso, altura)) {
+        alert("Por favor, insira valores válidos.")
+        return;
+}
 
+const imc =peso/ (altura *altura);
 
-do {
-    peso = parseFloat(prompt("Digite seu peso (em kg): "));
-    altura = parseFloat(prompt("Digite sua altura (em metros): "));
-
-    if (valorInvalido(peso, altura)) {
-        console.log("Valor inválido! Tente novamente.");
-    }
-
-} while (valorInvalido(peso, altura));
-
-
-const imc = peso / (altura * altura);
 
 if (imc > 40)
-    console.log("Obesidade grau 3");
+    resultado.textContent ="Obesidade grau 3";
 else if (imc < 40 && imc >= 35)
-    console.log("Obesidade grau 2")
+    resultado.textContent ="Obesidade grau 2";
 else if (imc < 35 && imc >= 30)
-    console.log("Obesidade grau 1")
+    resultado.textContent ="Obesidade grau 1";
 else if (imc < 30 && imc >= 25)
-    console.log("Pré-Obesidade")
+    resultado.textContent ="Pré-Obesidade";
 else if (imc < 25 && imc >= 18.5)
-    console.log("Peso Normal")
+    resultado.textContent ="Peso Normal";
 else if (imc < 18.5)
-    console.log("Abaixo do peso")
+    resultado.textContent ="Abaixo do peso";
 
-process.exit();
+})
